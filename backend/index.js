157fae53662c925628d01b683f2072cfba60570d
AlfreoEv2 <alfredosalvadorestradavega@hotmail.com>
2024-02-27 00:35:41 +0000

    Setup (Print something in localhost:5555)

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import {PORT} from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(200).send('Welcome to CodeCollab!');
+});
+
 app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
-})
+    console.log(`App is listening to port: ${PORT}`);
+});
