e27eccee1cae80db760a512f789c65f521b45c4a
A-dith-ya <awedithya@gmail.com>
2024-02-29 22:32:03 -0800

    Add toolbar component

---
+++ app/src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,14 @@
+import "./Toolbar.css";
+
+const Toolbar = () => {
+  return (
+    <div className="toolbar">
+      <button>File</button>
+      <button>Edit</button>
+      <button>View</button>
+      <button>Run</button>
+    </div>
+  );
+};
+
+export default Toolbar;
