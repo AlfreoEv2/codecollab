c17fbecdcd718a10bcead9014ad6416f494b1b0c
AlfreoEv2 <alfredosalvadorestradavega@hotmail.com>
2024-03-01 04:56:15 +0000

    Models (User schema)

---
+++ api/models/User.ts
@@ -0,0 +1,20 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  profilePicture?: string;
+  creationDate: Date;
+}
+
+const UserSchema: Schema = new Schema<IUser>({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  creationDate: { type: Date, default: Date.now },
+});
+
+const UserModel = mongoose.model<IUser>('User', UserSchema);
+
+export default UserModel;
