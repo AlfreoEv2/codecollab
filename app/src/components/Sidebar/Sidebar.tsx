fdff97a870359f97c7b04c5367683fb1facd78a0
A-dith-ya <awedithya@gmail.com>
2024-03-14 20:27:10 -0700

    Implement sidebar menu activation

--- app/src/components/Sidebar/Sidebar.tsx
+++ app/src/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FileNavigator from "../../components/Sidebar/FileNavigator";
 import { SidebarProps } from "../../interfaces/SidebarInterface";
 import "./Sidebar.css";
 
 const Sidebar = ({ files }: SidebarProps) => {
+  const [activeMenu, setActiveMenu] = useState<string | null>(null);
+
+  const handleFileIconClick = () => {
+    setActiveMenu(activeMenu === "file" ? null : "file");
+  };
+
   return (
     <div className="sidebar-container">
       <div className="sidebar">
-        <FontAwesomeIcon icon={["fas", "file"]} className="sidebar-icon" />
+        <FontAwesomeIcon
+          icon={["fas", "file"]}
+          className="sidebar-icon"
+          onClick={handleFileIconClick}
+        />
         <FontAwesomeIcon
           icon={["fas", "code-branch"]}
           className="sidebar-icon"
         />
       </div>
-      <FileNavigator files={files} />
+      {activeMenu === "file" && <FileNavigator files={files} />}
     </div>
   );
 };
