f0f6f33d78604948348feba83dda84646db1d5bf
A-dith-ya <awedithya@gmail.com>
2024-03-16 00:06:37 -0700

    Refactor useLine hook into useLineHandlers custom hook

--- app/src/pages/Editor/Editor.tsx
+++ app/src/pages/Editor/Editor.tsx
@@ -5,60 +5,13 @@ import CodeArea from "../../components/CodeArea/CodeArea";
 import Console from "../../components/Console/Console";
 import { FileOrFolder } from "../../interfaces/SidebarInterface";
 import menuItems from "../../data/menuItems.json";
+import useLineHandlers from "../../hooks/useLineHandler";
 import "./Editor.css";
 
 const Editor = () => {
   const [files, setFiles] = useState<FileOrFolder[]>([]);
-  const [lines, setLines] = useState<string[]>([""]);
   const [command, setCommand] = useState<string>("");
-
-  const handleLineChange = (
-    e: React.FormEvent<HTMLDivElement>,
-    index: number
-  ) => {
-    const newLines = [...lines];
-    newLines[index] = e.currentTarget.innerHTML;
-    setLines(newLines);
-  };
-
-  const handleLineOnEnter = (
-    e: React.KeyboardEvent<HTMLDivElement>,
-    index: number
-  ) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-
-      const sel = window.getSelection();
-      if (sel) {
-        const selRange = sel.getRangeAt(0);
-        // Get the text after the caret position
-        const textAfterCaret =
-          selRange.endContainer.textContent?.slice(selRange.endOffset) || "";
-
-        setLines((prevLines) => {
-          const newLines = [...prevLines];
-          // Update current line to include text until the caret position
-          newLines[index] = newLines[index].slice(0, selRange.endOffset);
-          // Insert a new line after the current line with the text
-          newLines.splice(index + 1, 0, textAfterCaret);
-          return newLines;
-        });
-
-        setTimeout(() => {
-          // Select the next div
-          const nextLine =
-            document.querySelectorAll(".line-content")[index + 1];
-          if (nextLine) {
-            const range = document.createRange();
-            range.setStart(nextLine, 0);
-            range.collapse(true);
-            sel.removeAllRanges();
-            sel.addRange(range);
-          }
-        }, 0);
-      }
-    }
-  };
+  const [lines, handleLineChange, handleLineOnEnter] = useLineHandlers([""]);
 
   return (
     <div>
